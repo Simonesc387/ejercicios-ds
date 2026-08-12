f847e15919b87d4fdccf3edf5e4673fbf0e55185
Simon Escobar <simonesc222@gmail.com>
2026-08-12 12:28:57 -0300

    ejercicio 4 SQLAlchemy

--- sqlalchemy/ejercicios.py
+++ sqlalchemy/ejercicios.py
@@ -28,6 +28,17 @@ class Profesor(Base):
     departamento_id: Mapped[int] = mapped_column(ForeignKey("departamentos.id"))
     
     departamento: Mapped[Departamento] = relationship(back_populates="profesores")
+    cursos: Mapped[List["Curso"]] = relationship(back_populates="profesor")
+    
+class Curso(Base):
+    __tablename__ = "cursos"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    titulo: Mapped[str] = mapped_column(String(100))
+    creditos: Mapped[int] = mapped_column()
+    
+    profesor_id: Mapped[int] = mapped_column(ForeignKey("profesores.id"))
+    profesor: Mapped[Profesor] = relationship(back_populates="cursos")
     
 def main():
     Base.metadata.create_all(engine)
@@ -38,12 +49,16 @@ def main():
         p1 = Profesor(id=1, nombre="Marcelo", email="marcelo@gmail.com", fecha_ingreso=datetime(2010, 2, 23), departamento_id=1)
         p2 = Profesor(id=2, nombre="Jose", email="jose@gmail.com", fecha_ingreso=datetime(2019, 10, 31), departamento_id=1)
         p3 = Profesor(id=3, nombre="Juan", email="juan@gmail.com", fecha_ingreso=datetime(2014, 8, 1), departamento_id=1)
+        cur1 = Curso(id=1, titulo="Algebra 1", creditos=10, profesor_id=1)
+        cur2 = Curso(id=2, titulo="Analisis 1", creditos=15, profesor_id=2)
         session.add(dep1)
         session.add(dep2)
         session.add(dep3)
         session.add(p1)
         session.add(p2)
         session.add(p3)
+        session.add(cur1)
+        session.add(cur2)
         session.commit()
         
     with Session(engine) as session:
@@ -56,7 +71,13 @@ def main():
         stmt = select(Profesor)
         profesores = session.scalars(stmt)
         for prof in profesores:
-            print(f"Profesor: {prof.nombre}, email: {prof.email}, fecha de ingreso: {prof.fecha_ingreso}, departamento: {prof.departamento.nombre}")
+            print(f"Profesor: {prof.nombre}, email: {prof.email}, fecha de ingreso: {prof.fecha_ingreso}, departamento: {prof.departamento.nombre}, cursos:")
+            for cur in prof.cursos:
+                print(cur.titulo)
+        stmt = select(Curso)
+        cursos = session.scalars(stmt)
+        for cur in cursos:
+            print(f"Curso: {cur.titulo}, creditos: {cur.creditos}, profesor: {cur.profesor.nombre}")
     
     
     
