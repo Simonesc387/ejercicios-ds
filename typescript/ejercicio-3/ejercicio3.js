bc4a1aa305fb08f44a57610780a460306d7f4ce9
Simesc <simonesc222@gmail.com>
2026-08-30 20:36:13 -0300

    Ejercicio 3 TS

---
+++ typescript/ejercicio-3/ejercicio3.js
@@ -0,0 +1,7 @@
+"use strict";
+const persona = {
+    nombre: "Juan",
+    edad: 23
+};
+console.log(persona.nombre);
+console.log(persona.edad);
---
+++ typescript/ejercicio-3/ejercicio3.ts
@@ -0,0 +1,12 @@
+interface Persona {
+    nombre: string;
+    edad: number;
+}
+
+const persona: Persona = {
+    nombre: "Juan",
+    edad: 23
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
